6e8dd60646858f3cbae50e83767393dd4aa380f9
srodax <ms@maksymiliansroda.pl>
2022-03-24 14:02:33 +0100

    now buckets several y columns

--- Bucket.py
+++ Bucket.py
@@ -8,7 +8,7 @@ import argparse
 #***********************************************************************
 # Parsing arguments
 #***********************************************************************
-parser = argparse.ArgumentParser(description='Bin data into specified range.')
+parser = argparse.ArgumentParser(description='Bin data into specified range. If the input has more than 2 columns, the 1st one is treated as x and the remaining as different y values.')
 parser.add_argument('min_x', type=float, nargs='?', help='Starting value of x.', default=-4.0)
 parser.add_argument('max_x', type=float, nargs='?', help='Stop value of x.', default=8.0)
 parser.add_argument('step_x', type=float, nargs='?', help='Increment of x.', default=0.02)
@@ -23,15 +23,17 @@ data = np.loadtxt(fileinput.input(args.files), dtype=float)
 bin_x = np.linspace( args.min_x, args.max_x, int(abs(args.max_x - args.min_x)/args.step_x) )
 
 # binning with scipy
-ret = stats.binned_statistic(data[:,0], data[:,1], 'mean', bins=[bin_x])
+datanew=[]
+# getting bin centers as new x values, which we keep as first column of our data
+datanew.append((bin_x[1:] + bin_x[:-1])/2)
 
-# preparing new yvalues, with 0s in empty bins
-ynew = ret.statistic
-ynew[np.isnan(ynew)] = 0.0
+for i in range(1,len(data[0,:])):
+    ret = stats.binned_statistic(data[:,0], data[:,i], 'mean', bins=[bin_x])
 
-# getting bin centers as new x values
-xnew = (bin_x[1:] + bin_x[:-1])/2
+    # preparing new yvalues, with 0s in empty bins
+    datanew.append(ret.statistic)
+    datanew[i][np.isnan(datanew[i])] = 0.0
 
 # printing the result to std out
-np.savetxt(sys.stdout, np.c_[xnew, ynew], newline='\n')
+np.savetxt(sys.stdout, np.stack(datanew,axis=1), newline='\n')
 
